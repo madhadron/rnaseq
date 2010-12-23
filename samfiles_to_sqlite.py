519c39ea621dbbe2470155d5200eeebf551ddffd
Frederick Ross <ross@ptbbpc1.epfl.ch>
2010-12-23 16:18:22 +0100

    Made samfiles_to_sqlite.py load one sample group at a time.

--- samfiles_to_sqlite.py
+++ samfiles_to_sqlite.py
@@ -2,10 +2,11 @@
 samfiles_to_sqlite.py
 by Fred Ross, <madhadron@gmail.com>
 
-Load two lists of SAM/BAM files into an SQLite database, calculating
-their leftsite depth on each transcript and the multiplicities.  The
-resulting database is ready to be handed to find_subproblems.py and to
-inference.py, both of which will treat it as read only.
+Load a list of SAM/BAM files into an SQLite database as a group,
+calculating their leftsite depth on each transcript and the
+multiplicities.  The resulting database is ready to be handed to
+find_subproblems.py and to inference.py, both of which will treat it
+as read only.
 """
 
 import getopt
@@ -14,15 +15,17 @@ import sys
 import pysam
 import sqlite3
 
-usage = """samfiles_to_sqlite.py [-vh] [-l readlen] condition1 condition2 db
+usage = """samfiles_to_sqlite.py [-vh] [-l readlen] (-c|-x) [-g group] db samfiles ...
 
 -v           Run verbosely
 -h           Print this message and exit
 -l readlen   Reads have length 'readlen' in SAM/BAM files
-condition1/2 Comma separated lists of SAM/BAM files, each containing
-             the aligned reads from one biological sample.  condition1
-             corresponds to one condition in the one way linear model,
-             condition2 to the other.
+-c|-x        -c makes this group a control, -x makes it an experimental sample
+-g group     Insert the samfiles into the database with group ID 'group'.  If
+             omitted, just uses the next free value in the database.
+db           The SQLite3 database to write to.
+samfiles     A list of SAM/BAM files, each containing the aligned reads
+             from one biological sample.
 """
 
 def initialize_database(db):
@@ -76,9 +79,17 @@ def initialize_database(db):
     db.commit()
     vmsg("Initialized database.")
 
-def insert_sample_group(db, label, is_control):
-    db.execute("""insert into sample_group (label,is_control)
-                  values (?,?)""", (label, is_control))
+def insert_sample_group(db, label, is_control, group_id=None):
+    if group_id != None:
+        x = db.execute("""select id from sample_group where id=?""", (group_id,)).fetchall()
+        if x != None:
+            raise ValueError("Group %d already exists in database." % group_id)
+    elif group_id == None:
+        db.execute("""insert into sample_group (label,is_control)
+                      values (?,?)""", (label, is_control))
+    else:
+        db.execute("""insert into sample_group (id,label,is_control)
+                      values (?,?,?)""", (group_id, label, is_control))
     (sample_group,) = db.execute("""select last_insert_rowid()""").fetchone()
     vmsg("Assigned sample group id %d to group with label %s" % (sample_group, label))
     return sample_group
@@ -195,6 +206,9 @@ class State(object):
     def __init__(self):
         self.verbose = False
         self.read_length = 38
+        self.is_control = None
+        self.group_id = None
+        self.group_label = ""
 
 state = State()
 
@@ -207,7 +221,11 @@ def main(argv=None):
         argv = sys.argv[1:]
     try:
         try:
-            opts, args = getopt.getopt(argv, "hvl:", ["help"])
+            opts, args = getopt.getopt(argv, "hvl:L:cxg:", 
+                                       ["help","read-length",
+                                        "group-label","control",
+                                        "experimental",
+                                        "group-id"])
         except getopt.error, message:
             raise Usage(message)
         for o, a in opts:
@@ -215,43 +233,53 @@ def main(argv=None):
                 print __doc__
                 print usage
                 sys.exit(0)
-            if o in ("-v", ):
+            elif o in ("-v", ):
                 state.verbose=True
                 print "Running verbosely."
-            if o in ("-l", ):
+            elif o in ("-l", "--read-length"):
                 try:
                     state.read_length = int(a)
                 except ValueError, v:
                     raise Usage("Read length must be an integer, found %s" % a)
+            elif o in ("-x", "--experimental"):
+                state.is_control = False
+            elif o in ("-c", "--control"):
+                if state.is_control == False:
+                    raise Usage("Can only specify one of -x and -c")
+                else:
+                    state.is_control = True
+            elif o in ("-g", "--group-id"):
+                try:
+                    state.group_id = int(a)
+                except ValueError, v:
+                    raise Usage("Group ID must be an integer, found %s" % a)
+            elif o in ("-L", "--group-label"):
+                state.group_label = a
             else:
                 raise Usage("Unhandled option: " + o)
-        if len(args) != 3:
-            raise Usage("samfiles_to_sqlite.py takes exactly three arguments.")
-        condition1_files = args[0].split(',')
-        for f in condition1_files:
+        if len(args) < 2:
+            raise Usage("samfiles_to_sqlite.py takes at least two arguments.")
+        if state.is_control == None:
+            raise Usage("Must specify one of -c or -x")
+
+        db_filename = args[0]
+        samfiles = args[1:]
+        for f in samfiles:
             if not(os.path.exists(f)):
                 raise Usage("Input file %s does not exist." % f)
 
-        condition2_files = args[1].split(',')
-        for f in condition2_files:
-            if not(os.path.exists(f)):
-                raise Usage("Input file %s does not exist." % f)
-
-        db_filename = args[2]
-        if os.path.exists(db_filename):
-            raise Usage("Database file %s already exists" % db_filename)
-
+        db_exists = os.path.exists(db_filename)
         db = sqlite3.connect(db_filename)
-        initialize_database(db)
-        control_sample_group = insert_sample_group(db, "Controls", True)
-        for f in condition1_files:
+        if not(db_exists):
+            initialize_database(db)
+
+        control_sample_group = insert_sample_group(db, state.group_label, 
+                                                   state.is_control, 
+                                                   state.group_id)
+
+        for f in samfiles:
             load_sam(db, f, control_sample_group)
-        other_sample_group = insert_sample_group(db, "Others", False)
-        for f in condition2_files:
-            load_sam(db, f, other_sample_group)
 
-
-    
         return 0
     except Usage, err:
         print >>sys.stderr, err.msg
