f1027f226a6ea173ed9d00ad745081a4d55a1953
Frederick Ross <ross@ptbbpc1.epfl.ch>
2010-12-27 11:47:53 +0100

    Added README file

--- rnaseq/__init__.py
+++ rnaseq/__init__.py
@@ -1,3 +1,7 @@
+"""
+A set of functions for doing RNASeq analysis.
+"""
+
 from model import build_model
 from load import initialize_database, insert_sample_group, load_sam
 from subproblems import find_subproblems
